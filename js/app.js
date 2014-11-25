0e6bf004b39e26f1cb5f8ac05db43233e618e773
Alex <Chavet>
2014-11-25 16:19:00 +1100

    JS Positioning the menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
-$(document).foundation();
+// $(document).foundation(); 
+jQuery(document).ready(function($) {
+var gap = ($(window).width()-1000)/2;
+$( "#floating" ).offset({ top: 20, left: gap });
+
+});
