3c8df474364a227a133419967303475601769bff
Alex <Chavet>
2014-11-25 18:28:15 +1100

    Skroller plugin

--- js/app.js
+++ js/app.js
@@ -6,3 +6,19 @@ var gap = ($(window).width()-1000)/2;
 $( "#floating" ).offset({ top: 20, left: gap });
 
 });
+
+
+
+//Skrollr Init 
+// https://ihatetomatoes.net/how-to-create-a-parallax-scrolling-website/
+
+
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+} )( jQuery );
