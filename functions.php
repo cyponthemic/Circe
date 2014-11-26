957ea1595714a6829a6bc34719b90a2dc1eae7e8
Alex <Chavet>
2014-11-26 16:31:29 +1100

    Templating

--- functions.php
+++ functions.php
@@ -49,4 +49,47 @@ add_action('wp_enqueue_scripts', 'load_cornerstone_child_scripts',50);
 
 
 
+// WOOCOMERCE DISABLE STYLES
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
+
+//WOOCOMERCE DROPDOWN
+
+// Place the following code in your theme's functions.php file
+// override the quantity input with a dropdown
+
+function woocommerce_quantity_input_dd() {
+
+global $product;
+$defaults = array(
+'input_name' => 'quantity',
+'input_value' => '1',
+'max_value' => apply_filters( 'woocommerce_quantity_input_max', '', $product ),
+'min_value' => apply_filters( 'woocommerce_quantity_input_min', '', $product ),
+'step' => apply_filters( 'woocommerce_quantity_input_step', '1', $product ),
+'style'	=> apply_filters( 'woocommerce_quantity_style', 'float:left; margin-right:10px;', $product )
+);
+if ( ! empty( $defaults['min_value'] ) )
+$min = $defaults['min_value'];
+else $min = 1;
+if ( ! empty( $defaults['max_value'] ) )
+$max = $defaults['max_value'];
+else $max = 20;
+if ( ! empty( $defaults['step'] ) )
+$step = $defaults['step'];
+else $step = 1;
+$options = '';
+for ( $count = $min; $count <= $max; $count = $count+$step ) {
+$options .= '<option value="' . $count . '">' . $count . '</option>';
+}
+echo '<div class="quantity_select" style="' . $defaults['style'] . '"><select name="' . esc_attr( $defaults['input_name'] ) . '" title="' . _x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) . '" class="qty">' . $options . '</select></div>';
+
+} 
+
+
+add_action('woocommerce_single_product_summary', 'Professionalrating',25);
+
+
+function Professionalrating(){
+echo get_the_ID();
+}
 ?>
--- header.php
+++ header.php
@@ -102,7 +102,13 @@ data-top-bottom="background-position: 50% 100%;"
 >
 	<div class="row">
 		<div class="large-offset-3 small-9 columns">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		
+		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) & is_shop()) : ?>
+			<h1 class="entry-title"><?php woocommerce_page_title(); ?></h1>		
+		<?php else: ?>
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		<?php endif; ?>
+				
 		</div>
 	</div>
 </header>
